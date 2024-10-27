c130dc6bd1f4ac6a5352c507eb31c79f9f69b1f6
COQUARD Cyrille <coquardcyr@gmail.com>
2024-10-27 23:38:37 +0100

    First example

---
+++ base/inc/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Launchpad;
+
+use Launchpad\Dependencies\LaunchpadCore\Container\AbstractServiceProvider;
+
+class ServiceProvider extends AbstractServiceProvider {
+
+	/**
+	 * Define your services.
+	 *
+	 * @return void
+	 */
+	protected function define() {
+		// Add your services.
+	}
+}
